c8a966896f22fc99f5a610045fcefee37b01425b
Anna Maria <aannamariabr@outlook.com>
2021-05-05 10:07:10 -0300

    refazendo link do carrinho

--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -2,7 +2,7 @@ const { Produto, sequelize } = require('../models');
 
 let produtosCarrinho = [];
 function findProduto(element, index, array) {
-  return element.id == this;
+  return (element.id == this);
 }
 
 const ProdutosController = {
@@ -13,27 +13,120 @@ const ProdutosController = {
 
   carrinho: async (request, response) => {
     let id = request.query.id;
+    
+    if (id) {
+      let index = produtosCarrinho.findIndex(findProduto, id);
 
-    let index = produtosCarrinho.findIndex(findProduto, id);
+      if (index >= 0) {
+        produtosCarrinho[index].qtde++;
+      } else {
+        const novoProduto = await Produto.findByPk(id);
+        novoProduto.qtde = 1;
+        produtosCarrinho.push(novoProduto);
+      }
 
-    if (index >= 0) {
-      produtosCarrinho[index].qtde++;
-    } else {
-      const novoProduto = await Produto.findByPk(id);
-      novoProduto.qtde = 1;
-      produtosCarrinho.push(novoProduto);
+      response.redirect('/produtos/carrinho');
     }
 
     let total = 0;
     produtosCarrinho.forEach((produto) => {
-      total += produto.valor * produto.qtde;
-    });
+       total += (produto.valor * produto.qtde);
+    } );
 
-    return response.render('carrinho', {
-      produtosCarrinho: produtosCarrinho,
-      valorTotal: total,
-    });
+    return response.render('carrinho', { produtosCarrinho: produtosCarrinho, valorTotal: total });
   },
+  
+
+ 
+  //------------------ todas flores----------------------------------------
+  suculenta: async (request, response) => {
+    return response.render('todasFlores/suculenta');
+  },
+  crisantemo: (request, response) => {
+    return response.render('todasFlores/crisantemo');
+  },
+  girassol: (request, response) => {
+    return response.render('todasFlores/girassol');
+  },
+  hortensia: (request, response) => {
+    return response.render('todasFlores/hortensia');
+  },
+  jasmim: (request, response) => {
+    return response.render('todasFlores/jasmim');
+  },
+  lirios: (request, response) => {
+    return response.render('todasFlores/lirios');
+  },
+  margarida: (request, response) => {
+    return response.render('todasFlores/margarida');
+  },
+  rosa: (request, response) => {
+    return response.render('todasFlores/rosa');
+  },
+  tulipa: (request, response) => {
+    return response.render('todasFlores/tulipa');
+  },
+  violeta: (request, response) => {
+    return response.render('todasFlores/violeta');
+  },
+  //-------------------rotas buques-----------------------
+  buqueRC: (request, response) => {
+    return response.render('todosBuques/buqueRosaC');
+  },
+  buqueBeleza: (request, response) => {
+    return response.render('todosBuques/buqueBeleza');
+  },
+  buqueFlorC: (request, response) => {
+    return response.render('todosBuques/buqueFlorC');
+  },
+  buqueMisto: (request, response) => {
+    return response.render('todosBuques/buqueMisto');
+  },
+  buqueMixRosa: (request, response) => {
+    return response.render('todosBuques/buqueMixRosas');
+  },
+  buqueOrqH: (request, response) => {
+    return response.render('todosBuques/buqueOH');
+  },
+  buqueR: (request, response) => {
+    return response.render('todosBuques/buqueRosas');
+  },
+  buquePrima: (request, response) => {
+    return response.render('todosBuques/buquePrimavera');
+  },
+  buqueRN: (request, response) => {
+    return response.render('todosBuques/buqueRosasN');
+  },
+  //-------- --------------Ocasiões-----------------------------------------------
+  diaMaes: (request, response) => {
+    return response.render('todasOcasioes/diaMaes');
+  },
+  diaMaesMisto: (request, response) => {
+    return response.render('todasOcasioes/mistoMaes');
+  },
+  namorados: (request, response) => {
+    return response.render('todasOcasioes/diaNamorados');
+  },
+  namoradosMisto: (request, response) => {
+    return response.render('todasOcasioes/mistoNamorados');
+  },
+  encontro: (request, response) => {
+    return response.render('todasOcasioes/primeiroEncontro');
+  },
+  encontroMisto: (request, response) => {
+    return response.render('todasOcasioes/mistoEncontro');
+  },
+  diaMulher: (request, response) => {
+    return response.render('todasOcasioes/diaMulheres');
+  },
+  casamento: (request, response) => {
+    return response.render('todasOcasioes/pedidoCasamento');
+  },
+  aniversario: (request, response) => {
+    return response.render('todasOcasioes/aniversario');
+  },
+
+  //----------------------- fim exibição produtos-------------------------------
 
   create: async (req, res) => {
     let {
